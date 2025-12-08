47341e8f6fea1bce0a0786b6e18878cb2b749c71
seanmerrow-rh <smerrow@redhat.com>
2025-12-08 16:29:44 -0500

    added comments

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,24 +5,36 @@ read -p "Enter the URL for the cluster API (ie. https://api.my-cluster.my-org.co
 read -s -p "Enter your access token from the OpenShift cluster: " token
 
 # Log in to the cluster
+echo "Logging in to the cluster"
 oc login --token=${token} --server=${api}
+echo
 
 # Change to the lakefs project. It must be created prior to running this script
+echo "Setting project to lakefs"
 oc project lakefs
+echo
 
 # Deploy minio
+echo "Deploying MinIO"
 oc apply -f ./minio-for-lakefs.yaml
 sleep 30
+echo
 
 # Create lakeFS config and storage in a config map
+echo "Creating configmap with lakeFS configuration"
 oc apply -f ./lakefs-config-job.yaml
 sleep 10
+echo
 
 # Deploy lakeFS
+echo "Deploying lakeFS with configuration in configmap"
 helm install my-lakefs ./helm/lakefs
 sleep 30
+echo
 
 # Create repos in lakeFS, thereby creating storage buckets in Minio
+echo "Creating data repos in lakeFS"
 oc apply -f ./lakefs-repos-job.yaml
 sleep 30
-
+echo
+echo "Done!"
