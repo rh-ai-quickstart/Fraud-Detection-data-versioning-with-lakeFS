ffc0f0ac1d457f058ff78cb81a7e211938274816
seanmerrow-rh <smerrow@redhat.com>
2025-12-10 09:49:35 -0500

    update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,6 +12,17 @@ usage_and_exit () {
     exit 1
 }
 
+error_and_exit () {
+    echo
+    echo $1
+    echo
+    echo "To clean up for another attempt, use:"
+    echo
+    echo "oc delete project $1"
+    echo
+    exit 1
+}
+
 # Validate a project name, and only that, is included
 if [ $# -lt 1 ]; then
     usage_and_exit "Must include project to deploy to"
@@ -19,14 +30,16 @@ elif [ $# -gt 1 ]; then
     usage_and_exit "Too many arguments"
 fi
 
+project=$1
+
 # Create the project if it doesn't already exist
-oc get project $1 > /dev/null 2>&1
+oc get project $project > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "$1 project already exists. Switching to it."
+    echo "$project project already exists. Switching to it."
     oc project lakefs
 else
-    echo "Creating new project $1 and switching to it."
-    oc new-project $1
+    echo "Creating new project $project and switching to it."
+    oc new-project $project
 fi
 echo
 
@@ -34,6 +47,9 @@ echo
 echo "Deploying MinIO and creating storage buckets"
 echo
 oc apply -f ./manifests/minio-for-lakefs.yaml
+if [ $? -ne 0 ]; then
+    error_and_exit $project "Error: Failed to apply minio-for-lakefs.yaml"
+fi
 sleep 30
 echo
 
@@ -41,6 +57,9 @@ echo
 echo "Creating configmap with lakeFS configuration"
 echo
 oc apply -f ./manifests/lakefs-config-job.yaml
+if [ $? -ne 0 ]; then
+    error_and_exit $project "Error: Failed to apply lakefs-config-job.yaml"
+fi
 sleep 10
 echo
 
@@ -48,6 +67,9 @@ echo
 echo "Deploying lakeFS with configuration in configmap"
 echo
 helm install my-lakefs ./helm/lakefs
+if [ $? -ne 0 ]; then
+    error_and_exit $project "Error: Failed to install my-lakefs helm chart"
+fi
 sleep 30
 echo
 
@@ -55,6 +77,9 @@ echo
 echo "Creating data repos in lakeFS"
 echo
 oc apply -f ./manifests/lakefs-repos-job.yaml
+if [ $? -ne 0 ]; then
+    error_and_exit $project "Error: Failed to apply lakefs-repos-job.yaml"
+fi
 sleep 30
 echo
 echo "Done!"
